a3c598a2a8a2f46be6ca009f8d8a18aefef3a3cd
Lukksky <lucassilvamatos94@gmail.com>
2024-09-15 22:50:50 -0300

    v3 - Inclusão das imagens personalizadas

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ function verificar(){
     var comparaComb = (precoE / precoG).toFixed(2)
     var porCentagem = comparaComb * 100
     var msg = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
 
     if (comparaComb >= 0 && comparaComb <= 0.70) {
         msg = `O Etanol é mais econômico!</br> É mais vantajoso quando está custando até 70% do preço da Gasolina. </br> E a diferença foi de ${porCentagem}%`
+        img.setAttribute('src', 'etanol.jpg')
         
     
     } else if (comparaComb > 0.70 && comparaComb <= 3) {
         msg = `A Gasolina é mais econômica!</br> É mais vantajosa quando está custando acima de 70% do preço do Etanol. </br> A diferença foi de ${porCentagem}%`
-        
+        img.setAttribute('src', 'gasolina.jpg')
     
     } else {
         msg = 'Erro, valide o valor de entrada e tente novamente!'
@@ -21,4 +24,5 @@ function verificar(){
 
     resposta.style.padding = '8px'
     resposta.innerHTML = `${msg}`
+    resposta.appendChild(img)
 }
